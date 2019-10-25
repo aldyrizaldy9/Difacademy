2decde5cac310cdf8ba8fe9b775f79195d7ec762
Aldy <rizaldy.ilham@gmail.com>
2019-10-26 00:09:17 +0700

    tampilan admin udah selain notif

--- app/src/main/java/com/example/aldy/difacademy/OpVideoBlendedCourseActivity.java
+++ app/src/main/java/com/example/aldy/difacademy/OpVideoBlendedCourseActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class OpNotificationActivity extends AppCompatActivity {
+public class OpVideoBlendedCourseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_op_notification);
+        setContentView(R.layout.activity_op_video_blended_course);
     }
 }
