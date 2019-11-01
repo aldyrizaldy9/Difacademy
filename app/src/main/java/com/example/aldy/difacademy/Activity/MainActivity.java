82fa39faf12dfb59cd586fdb11f7666f6727bdcc
Muhammad Budi fauzan <budifauzan1997@gmail.com>
2019-11-01 12:45:54 +0700

    settings

--- app/src/main/java/com/example/aldy/difacademy/Activity/MainActivity.java
+++ app/src/main/java/com/example/aldy/difacademy/Activity/MainActivity.java
@@ -1,16 +1,91 @@
 package com.example.aldy.difacademy.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.aldy.difacademy.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    ConstraintLayout clSettings, clOngoing;
+    ImageView ivKelasGratis, ivKelasOnline, ivKelasCampuran;
+    Button btnBeritaLainnya;
+    TextView tvDiikutiSemua;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findView();
+        onClick();
+    }
+
+    private void findView() {
+        clSettings = findViewById(R.id.cl_main_settings);
+        clOngoing = findViewById(R.id.cl_main_ongoing_container);
+        ivKelasGratis = findViewById(R.id.iv_main_kelas_gratis);
+        ivKelasOnline = findViewById(R.id.iv_main_kelas_online);
+        ivKelasCampuran = findViewById(R.id.iv_main_kelas_campuran);
+        btnBeritaLainnya = findViewById(R.id.btn_main_berita_lainnya);
+        tvDiikutiSemua = findViewById(R.id.tv_main_diikuti_semua);
+    }
+
+    private void onClick() {
+        clSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+        clOngoing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListVideoCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+        ivKelasGratis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FreeCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+        ivKelasOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Fitur ini belum tersedia, stay tune :)", Toast.LENGTH_SHORT).show();
+            }
+        });
+        ivKelasCampuran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BlendedCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnBeritaLainnya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ListNewsActivity.class);
+                startActivity(intent);
+            }
+        });
+        tvDiikutiSemua.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OngoingCourseActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/aldy/difacademy/Activity/SettingsActivity.java
+++ app/src/main/java/com/example/aldy/difacademy/Activity/SettingsActivity.java
@@ -1,16 +1,84 @@
 package com.example.aldy.difacademy.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.aldy.difacademy.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    ConstraintLayout clSunting, clKeluar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        findView();
+        onClick();
+    }
+
+    private void findView() {
+        clSunting = findViewById(R.id.cl_settings_sunting);
+        clKeluar = findViewById(R.id.cl_settings_keluar);
+    }
+
+    private void onClick() {
+        clSunting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingsActivity.this, EditProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        clKeluar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showKeluarDialog();
+            }
+        });
+    }
+
+    private void showKeluarDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+        builder.setMessage("Apakah anda yakin ingin keluar?");
+        builder.setTitle("Keluar");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Ya", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+//                pd.show();
+//
+//                auth.signOut();
+//
+//                SharedPreferences sharedPreferences = context.getSharedPreferences(SHARE_PREFS, MODE_PRIVATE);
+//                SharedPreferences.Editor editor = sharedPreferences.edit();
+//
+//                editor.putInt(STATE_USER_LOGGED_PREF, 0);
+//                editor.apply();
+//                STATE_USER_LOGGED = 0;
+//
+//                pd.dismiss();
+                Intent intent = new Intent(SettingsActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
+
+        builder.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
