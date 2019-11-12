146e9884a77de76e45aeeda0aa180ef8c3f6de38
Aldy <rizaldy.ilham@gmail.com>
2019-11-13 00:46:41 +0700

    blended course [create]

--- app/src/main/java/com/example/aldy/difacademy/Activity/OpBlendedCourseActivity.java
+++ app/src/main/java/com/example/aldy/difacademy/Activity/OpBlendedCourseActivity.java
@@ -1,16 +1,66 @@
 package com.example.aldy.difacademy.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.aldy.difacademy.R;
 
 public class OpBlendedCourseActivity extends AppCompatActivity {
+    private static final String TAG = "OpBlendedCourseActivity";
+
+    TextView tvNavbar;
+    ConstraintLayout clBack, clAdd;
+    ImageView imgBack, imgAdd;
+    RecyclerView rvBlended;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_op_blended_course);
+
+        initView();
+        onClick();
+        setRecyclerView();
+    }
+
+    private void initView(){
+        tvNavbar = findViewById(R.id.tv_navbar);
+        tvNavbar.setText("Blended Course");
+        clBack = findViewById(R.id.cl_icon1);
+        clBack.setVisibility(View.VISIBLE);
+        clBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        clAdd = findViewById(R.id.cl_icon3);
+        clAdd.setVisibility(View.VISIBLE);
+        imgBack = findViewById(R.id.img_icon1);
+        imgBack.setImageResource(R.drawable.ic_arrow_back);
+        imgAdd = findViewById(R.id.img_icon3);
+        imgAdd.setImageResource(R.drawable.ic_add);
+        rvBlended = findViewById(R.id.rv_op_blended);
+    }
+
+    private void onClick(){
+        clAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OpBlendedCourseActivity.this, OpAddBlendedCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void setRecyclerView(){
+
     }
 }
---
+++ app/src/main/java/com/example/aldy/difacademy/Model/BlendedCourseModel.java
@@ -0,0 +1,136 @@
+package com.example.aldy.difacademy.Model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class BlendedCourseModel implements Parcelable {
+    String documentId, title, description, tagId, tag, thumbnailUrl, gDriveUrl, harga;
+    long dateCreated;
+
+    public BlendedCourseModel(){}
+
+    public BlendedCourseModel(String title, String description, String tagId, String tag, String thumbnailUrl, String gDriveUrl, String harga, long dateCreated) {
+        this.title = title;
+        this.description = description;
+        this.tagId = tagId;
+        this.tag = tag;
+        this.thumbnailUrl = thumbnailUrl;
+        this.gDriveUrl = gDriveUrl;
+        this.harga = harga;
+        this.dateCreated = dateCreated;
+    }
+
+    protected BlendedCourseModel(Parcel in) {
+        documentId = in.readString();
+        title = in.readString();
+        description = in.readString();
+        tagId = in.readString();
+        tag = in.readString();
+        thumbnailUrl = in.readString();
+        gDriveUrl = in.readString();
+        harga = in.readString();
+        dateCreated = in.readLong();
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(documentId);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(tagId);
+        dest.writeString(tag);
+        dest.writeString(thumbnailUrl);
+        dest.writeString(gDriveUrl);
+        dest.writeString(harga);
+        dest.writeLong(dateCreated);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Creator<BlendedCourseModel> CREATOR = new Creator<BlendedCourseModel>() {
+        @Override
+        public BlendedCourseModel createFromParcel(Parcel in) {
+            return new BlendedCourseModel(in);
+        }
+
+        @Override
+        public BlendedCourseModel[] newArray(int size) {
+            return new BlendedCourseModel[size];
+        }
+    };
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTagId() {
+        return tagId;
+    }
+
+    public void setTagId(String tagId) {
+        this.tagId = tagId;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public String getgDriveUrl() {
+        return gDriveUrl;
+    }
+
+    public void setgDriveUrl(String gDriveUrl) {
+        this.gDriveUrl = gDriveUrl;
+    }
+
+    public String getHarga() {
+        return harga;
+    }
+
+    public void setHarga(String harga) {
+        this.harga = harga;
+    }
+
+    public long getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(long dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+}
