2408335e389bfd0b1f3a9f6066c938284875d4a2
Aldy <rizaldy.ilham@gmail.com>
2020-06-05 13:17:36 +0700

    minor bug register

--- app/src/main/java/com/example/aldy/difacademy/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/aldy/difacademy/Activity/RegisterActivity.java
@@ -148,9 +148,11 @@ public class RegisterActivity extends AppCompatActivity {
                                     .addOnFailureListener(new OnFailureListener() {
                                         @Override
                                         public void onFailure(@NonNull Exception e) {
+                                            pd.dismiss();
                                         }
                                     });
                         } else {
+                            pd.dismiss();
                             Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
